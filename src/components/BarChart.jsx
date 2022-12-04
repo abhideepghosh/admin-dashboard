0698c0a0cb0f908da5b6b14479453deee565bf5f
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-12-04 12:42:19 +0530

    pie chart completed

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -39,7 +39,7 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["production", "sales", "sandwich", "kebab", "fries", "donut"]}
+      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
       indexBy="country"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
