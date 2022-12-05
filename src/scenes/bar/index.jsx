82925403e7ca218b18330a860be9402cf2734d71
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-12-05 16:35:53 +0530

    Bug Fix

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -5,7 +5,7 @@ import BarChart from "../../components/BarChart";
 const Bar = () => {
   return (
     <Box m="20px">
-      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <Header title="Bar Chart" subtitle="Car Sales Bar Chart" />
       <Box height="75vh">
         <BarChart />
       </Box>
