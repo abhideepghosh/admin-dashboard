165d8adb1a6c3c53a9dc92154a6ea0e291e4cf63
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-12-05 16:14:51 +0530

    FAQ Section Built

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -16,66 +16,68 @@ const FAQ = () => {
 
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            An Important Question
+          <Typography color={colors.blueAccent[300]} variant="h5">
+            What is this project about?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            This project is an attempt to create an Industry Level Admin
+            Dashboard. It uses features like Data Grids, Charts and Calendar
+            with complete functionality to achieve the desired outcome.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            Another Important Question
+          <Typography color={colors.blueAccent[300]} variant="h5">
+            Team Behind this project?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            The Project Was Built By Abhideep Ghosh And Swati Sharma.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            Your Favorite Question
+          <Typography color={colors.blueAccent[300]} variant="h5">
+            What are the Tech Stack used in this project ?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            The languages used are HTML, CSS and JavaScript. The backbone of
+            this project is built on React.js Library.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            Some Random Question
+          <Typography color={colors.blueAccent[300]} variant="h5">
+            Which Modules are used in the project ?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Modules Used: Material UI, mui Icons, mui Data Grid,
+            react-router-dom, react-pro-sidebar, formik, yup, fullcalendar,
+            fullcalendar timegrid, fullcalendar list, nivo graphs
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            The Final Question
+          <Typography color={colors.blueAccent[300]} variant="h5">
+            What Funtionality Does this project holds?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Fully functional calendar with event management, datagrids with
+            filters and sorting functionality, dashboard, bar graph, line graph,
+            pie chart and world geography chart.
           </Typography>
         </AccordionDetails>
       </Accordion>
